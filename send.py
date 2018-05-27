a530a6cee06fe6603ae1ba4d36d44fc145bff1e3
shubhamchoubey <shubhamchoubey447@gmail.com>
2018-05-27 19:57:00 +0530

    encrypted chatting system

--- send.py
+++ send.py
@@ -2,8 +2,9 @@
 
 import socket
 import _thread
+import base64
 
-send_ip="127.0.0.1"
+send_ip="127.24.25.1"
 send_port=9999
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind((send_ip,send_port))
@@ -12,17 +13,20 @@ s.bind((send_ip,send_port))
 
 def send():
 	while True:
-		msg=input().encode(encoding='utf-8', errors='strict')
+		encrypt=base64.b64encode
+		msg=encrypt(input().encode(encoding='utf-8', errors='strict'))
 
-		s.sendto(msg,("127.0.0.1",8888))
+		s.sendto(msg,("127.24.25.30",8888))
 	
 
 def recv():
 	while True:
 
 		recv_content=s.recvfrom(1000)
-
-		string=recv_content[0].decode(encoding='utf-8', errors='strict')
+#		print(recv_content)
+		decrypt=base64.b64decode(recv_content[0])
+		
+		string=decrypt.decode(encoding='utf-8', errors='strict')
 
 		print(string)
 
