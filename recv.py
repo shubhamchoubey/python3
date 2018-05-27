d86f88f041ebf53082e392b34f740ae9b9de5771
shubhamchoubey <shubhamchoubey447@gmail.com>
2018-05-27 17:37:27 +0530

    python socket with multithreading

--- recv.py
+++ recv.py
@@ -2,25 +2,48 @@
 
 import socket
 import time
-import collections as cl 
+import _thread
+
 rec_ip="127.0.0.1"
 rec_port=8888
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 s.bind((rec_ip,rec_port))
-count=0
-while True:
-		
-	recv_content=s.recvfrom(1000)	
-	
-	string=recv_content[0].decode(encoding='utf-8', errors='strict').split()
-	count=cl.Counter(string)
-	elements=sorted(count)
-	for i in elements:
-		print(i,count[i])
 
-#	print(count)
-#	count=count+str
-#	time.sleep
-		
+def recv():
+	while True:
+		recv_content=s.recvfrom(1000)	
+	
+		string=recv_content[0].decode(encoding='utf-8', errors='strict').split()
+		string_cor=""
+		for i in range(len(string)):
+			string_cor=string_cor+" "+string[i]
+			
+		print(string_cor)
+
+#	time.sleep(1)
+def send():
+	while True:
+
+		msg=input().encode(encoding='utf-8', errors='strict')
+	
+		s.sendto(msg,('127.0.0.1',9999))
+
+
+	
+_thread.start_new_thread(send,())
+_thread.start_new_thread(recv,())
+
+while True:
+	pass	
+
+	
+
+	
+	
+	
+
+
+
+
 	
